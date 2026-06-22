73a3413f9e1fae4ff7a11dee565b82daebcb2ccc
Viviane Aguiar <vivianeaguiarc@outlook.com>
2026-06-22 14:42:45 -0300

    chore: setup backend code quality tools

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+);
---
+++ src/server.ts
@@ -0,0 +1,1 @@
+console.log('SupportFlow backend is running');
